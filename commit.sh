9a385512196b2ce3084e96bd4106fb6e1d6baef0
azizyksl <114660791+azizyksl@users.noreply.github.com>
2023-05-03 13:40:29 +0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,7 +3,6 @@ AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
 git clone https://github.com/azizyksl/Deneme-Sync
-cd Deneme-Sync
 rm -rf .git
 
 cd ..
@@ -11,7 +10,7 @@ cd ..
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Deneme-Sync/_git/Deneme-Sync"
 git clone $GIT_CMD_REPOSITORY
 
-cp -r Deneme-Sync/* Deneme-Sync/
+cp -r * Deneme-Sync/
 
 cd Deneme-Sync
 
